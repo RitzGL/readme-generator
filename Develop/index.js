d153c87f15e8e7ec8b5b7df59f851d2b35e93a92
Ritz GL <maurigarzonlara@hotmail.com>
2021-05-05 21:02:28 +0800

    data reading from cli implemented, minor bug to be fixed

--- Develop/index.js
+++ Develop/index.js
@@ -3,73 +3,59 @@ const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: 'confirm',
-    name: 'toBeDelivered',
-    message: 'Is this for delivery?',
-    default: false,
-  },
-  {
-    type: 'list',
-    name: 'size',
-    message: 'What size do you need?',
-    choices: ['Large', 'Medium', 'Small'],
-    filter: function (val) {
-      return val.toLowerCase();
-    },
+    type: 'input',
+    name: 'title',
+    message: 'Enter README title: ',
   },
   {
     type: 'input',
-    name: 'quantity',
-    message: 'How many do you need?',
-    validate: function (value) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || 'Please enter a number';
-    },
-    filter: Number,
-  },
-  {
-    type: 'expand',
-    name: 'toppings',
-    message: 'What about the toppings?',
-    choices: [
-      {
-        key: 'p',
-        name: 'Pepperoni and cheese',
-        value: 'PepperoniCheese',
-      },
-      {
-        key: 'a',
-        name: 'All dressed',
-        value: 'alldressed',
-      },
-      {
-        key: 'w',
-        name: 'Hawaiian',
-        value: 'hawaiian',
-      },
-    ],
-  },
-  {
-    type: 'rawlist',
-    name: 'beverage',
-    message: 'You also get a free 2L beverage',
-    choices: ['Pepsi', '7up', 'Coke'],
+    name: 'description',
+    message: 'Briefly describe the application: ',
   },
   {
     type: 'input',
-    name: 'comments',
-    message: 'Any comments on your purchase experience?',
-    default: 'Nope, all good!',
+    name: 'instalDesc',
+    message: 'How do you install the application?',
+  },
+  {
+    type: 'input',
+    name: 'usageDesc',
+    message: 'Briefly describe how the application is used: ',
   },
   {
     type: 'list',
-    name: 'prize',
-    message: 'For leaving a comment, you get a freebie',
-    choices: ['cake', 'fries'],
+    name: 'license',
+    message: 'Choose license',
+    choices: ['MIT', 'GPLv3', 'AGPL'],
+  },
+  {
+    type: 'input',
+    name: 'licenseDesc',
+    message: 'Briefly describe the license: ',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Mention contributors: ',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Briefly describe testing strategy: ',
+  },
+  {
+    type: 'input',
+    name: 'ghName',
+    message: 'Enter GitHub name: ',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter email address: ',
     when: function (answers) {
-      return answers.comments !== 'Nope, all good!';
+      return answers.comments;
     },
-  },
+  }
 ];
 
 // TODO: Create a function to write README file
