c758003f9d203efdcf5a6eafde5300d10421bdb4
Ritz GL <maurigarzonlara@hotmail.com>
2021-05-05 20:16:55 +0800

    fixed indentation, started own implementation

--- Develop/index.js
+++ Develop/index.js
@@ -2,89 +2,74 @@
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'confirm',
-        name: 'toBeDelivered',
-        message: 'Is this for delivery?',
-        default: false,
+  {
+    type: 'confirm',
+    name: 'toBeDelivered',
+    message: 'Is this for delivery?',
+    default: false,
+  },
+  {
+    type: 'list',
+    name: 'size',
+    message: 'What size do you need?',
+    choices: ['Large', 'Medium', 'Small'],
+    filter: function (val) {
+      return val.toLowerCase();
+    },
+  },
+  {
+    type: 'input',
+    name: 'quantity',
+    message: 'How many do you need?',
+    validate: function (value) {
+      var valid = !isNaN(parseFloat(value));
+      return valid || 'Please enter a number';
+    },
+    filter: Number,
+  },
+  {
+    type: 'expand',
+    name: 'toppings',
+    message: 'What about the toppings?',
+    choices: [
+      {
+        key: 'p',
+        name: 'Pepperoni and cheese',
+        value: 'PepperoniCheese',
       },
       {
-        type: 'input',
-        name: 'phone',
-        message: "What's your phone number?",
-        validate: function (value) {
-          var pass = value.match(
-            /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
-          );
-          if (pass) {
-            return true;
-          }
-    
-          return 'Please enter a valid phone number';
-        },
+        key: 'a',
+        name: 'All dressed',
+        value: 'alldressed',
       },
       {
-        type: 'list',
-        name: 'size',
-        message: 'What size do you need?',
-        choices: ['Large', 'Medium', 'Small'],
-        filter: function (val) {
-          return val.toLowerCase();
-        },
+        key: 'w',
+        name: 'Hawaiian',
+        value: 'hawaiian',
       },
-      {
-        type: 'input',
-        name: 'quantity',
-        message: 'How many do you need?',
-        validate: function (value) {
-          var valid = !isNaN(parseFloat(value));
-          return valid || 'Please enter a number';
-        },
-        filter: Number,
-      },
-      {
-        type: 'expand',
-        name: 'toppings',
-        message: 'What about the toppings?',
-        choices: [
-          {
-            key: 'p',
-            name: 'Pepperoni and cheese',
-            value: 'PepperoniCheese',
-          },
-          {
-            key: 'a',
-            name: 'All dressed',
-            value: 'alldressed',
-          },
-          {
-            key: 'w',
-            name: 'Hawaiian',
-            value: 'hawaiian',
-          },
-        ],
-      },
-      {
-        type: 'rawlist',
-        name: 'beverage',
-        message: 'You also get a free 2L beverage',
-        choices: ['Pepsi', '7up', 'Coke'],
-      },
-      {
-        type: 'input',
-        name: 'comments',
-        message: 'Any comments on your purchase experience?',
-        default: 'Nope, all good!',
-      },
-      {
-        type: 'list',
-        name: 'prize',
-        message: 'For leaving a comment, you get a freebie',
-        choices: ['cake', 'fries'],
-        when: function (answers) {
-          return answers.comments !== 'Nope, all good!';
-        },
-      },
+    ],
+  },
+  {
+    type: 'rawlist',
+    name: 'beverage',
+    message: 'You also get a free 2L beverage',
+    choices: ['Pepsi', '7up', 'Coke'],
+  },
+  {
+    type: 'input',
+    name: 'comments',
+    message: 'Any comments on your purchase experience?',
+    default: 'Nope, all good!',
+  },
+  {
+    type: 'list',
+    name: 'prize',
+    message: 'For leaving a comment, you get a freebie',
+    choices: ['cake', 'fries'],
+    when: function (answers) {
+      return answers.comments !== 'Nope, all good!';
+    },
+  },
 ];
 
 // TODO: Create a function to write README file
@@ -92,10 +77,10 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-        console.log('\nOrder receipt:');
-        console.log(JSON.stringify(answers, null, '  '));
-      });
+  inquirer.prompt(questions).then((answers) => {
+    console.log('\nOrder receipt:');
+    console.log(JSON.stringify(answers, null, '  '));
+  });
 }
 
 // Function call to initialize app
